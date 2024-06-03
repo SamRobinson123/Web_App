489cde91088818eb5b5be27133a27893097fd6d4
SamRobinson123 <Samrobinson290225@gmail.com>
2024-06-02 21:53:31 -0600

    code change

--- WebApp.py
+++ WebApp.py
@@ -1,15 +1,9 @@
 from flask import Flask, redirect, url_for, render_template
 
-
 app = Flask(__name__)
-
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+    return render_template("index.html",content="Testing")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
